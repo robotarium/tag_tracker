3259fdce820112cf2d1b7372b2739fe0db30248f
pglotfel <pglotfel@gmail.com>
2016-07-26 13:07:19 -0400

    Added subscribe feature to mqtt client

--- mqttclient/src/mqttclient.hpp
+++ mqttclient/src/mqttclient.hpp
@@ -1,12 +1,18 @@
 #ifndef MQTT_CLIENT_H
 #define MQTT_CLIENT_H
 
+#include <unordered_map>
 #include <string>
 #include <mosquitto.h>
 #include <thread>
 #include <tsqueue.hpp>
 #include <promise.hpp>
 #include <iostream>
+#include <functional>
+
+class MQTTClient;
+
+MQTTClient* global_client;
 
 class MQTTClient {
 
@@ -25,6 +31,7 @@ private:
   std::string host;
   ThreadSafeQueue<std::shared_ptr<DataTuple>> q;
   std::shared_ptr<std::thread> runner;
+  std::unordered_map<std::string, std::function<void(std::string)>> subscriptions;
   int port;
   struct mosquitto *mosq;
 
@@ -33,6 +40,11 @@ public:
   MQTTClient(std::string host, int port) {
 
     ThreadSafeQueue<std::shared_ptr<DataTuple>> q();
+
+    // TODO: Fix worst singleton pattern ever
+    if(global_client == NULL) {
+      global_client = this;
+    }
 
     this->host = host;
     this->port = port;
@@ -54,6 +66,8 @@ public:
       throw 2;
     }
 
+    //Set message callback and start the loop!
+    mosquitto_message_callback_set(mosq, &MQTTClient::message_callback);
   	mosquitto_loop_start(mosq);
   }
 
@@ -81,11 +95,30 @@ public:
   }
 
   //TODO: Implement subscribe method!
-  void subscribe(std::string topic) { }
+  void subscribe(std::string topic, std::function<void(std::string)> f) {
+    //Struct, ?, topic string, QOS
+    global_client->subscriptions[topic] = f;
+    mosquitto_subscribe(mosq, NULL, topic.c_str(), 1);
+  }
+
+  void unsubscribe(std::string topic) {
+    mosquitto_unsubscribe(mosq, NULL, topic.c_str());
+    global_client->subscriptions.erase(topic.c_str());
+  }
 
   void async_publish(std::string topic, std::string message) {
     std::shared_ptr<DataTuple> msg = std::make_shared<DataTuple>(topic, message);
     q.enqueue(msg);
   }
+
+private:
+
+  // Callback for handling incoming MQTT messages
+  static void message_callback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message) {
+    if(global_client->subscriptions.find(message->topic) != global_client->subscriptions.end()) {
+      global_client->subscriptions[message->topic](std::string((char*) message->payload));
+    }
+  }
+
 };
 #endif
--- mqttclient/test/mqttclienttest.cpp
+++ mqttclient/test/mqttclienttest.cpp
@@ -1,11 +1,39 @@
 #include <mqttclient.hpp>
 #include <iostream>
+
+
+void callback(std::string message) {
+  std::cout << message << std::endl;
+}
+
+//std::function<void(std::string)> stdf_callback = &callback;
+
+std::function<void(std::string)> stdf_callback = &callback;
+
 int main(void) {
 
-  MQTTClient m("localhost", 8080);
+  MQTTClient m("localhost", 1884);
   m.start();
 
+  m.subscribe("/tracker/overhead", stdf_callback);
+
+  std::cin.ignore();
+
   m.async_publish("/tracker/overhead", "hi!");
+  m.async_publish("/tracker/overhead", "hi!");
+  m.async_publish("/tracker/overhead", "hi!");
+
+  std::cin.ignore();
+
+  m.unsubscribe("/tracker/overhead");
+
+  std::cin.ignore();
+
+  m.async_publish("/tracker/overhead", "hi!");
+  m.async_publish("/tracker/overhead", "hi!");
+  m.async_publish("/tracker/overhead", "hi!");
+
+  std::cin.ignore();
 
   return 0;
 }
