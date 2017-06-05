22f2ea9101831ba0f0df80c192031added1571be
pglotfel <pglotfel@gmail.com>
2017-06-05 15:32:34 -0400

    minor changes

--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,7 @@
 
 # Make sure that 'xhost +' has been run before this script!!!!
 
-docker run -ti --rm \
+docker run -ti -d \
 	-e DISPLAY=$DISPLAY \
 	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 	-v /home/robotarium/Git/RobotariumRepositories/robotarium/serverBackend/mqttExtravaganza/tracker_alias.json:/home/robotarium/Git/RobotariumRepositories/robotarium/serverBackend/mqttExtravaganza/tracker_alias.json \
