1785320eee898ba2f6b97bbc741a1aafbfcee155
pglotfel <pglotfel@gmail.com>
2017-06-05 15:43:09 -0400

    Now there's some myst. delay...

--- src/detect_markers_opencv_3.0.0.cpp
+++ src/detect_markers_opencv_3.0.0.cpp
@@ -613,7 +613,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* Add corner refinement in markers */
-	detectorParams->cornerRefinementMethod = 0;
+	detectorParams->cornerRefinementMethod = 1;
 
 	/* Instantiate dictionary containing tags */
 	Ptr<aruco::Dictionary> dictionary =
