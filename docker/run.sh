632a487ecc100c3c9cb5ca39d768a726235f3dc8
dpickem <dpickem3@gatech.edu>
2017-06-19 16:30:00 -0400

    added docker and modified CMakeLists to match the Dockerization branch

---
+++ docker/run.sh
@@ -0,0 +1,11 @@
+#/bin/bash 
+
+# Make sure that 'xhost +' has been run before this script!!!!
+
+docker run -ti -d \
+	-e DISPLAY=$DISPLAY \
+	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+	-v /home/robotarium/Git/RobotariumRepositories/robotarium/serverBackend/mqttExtravaganza/tracker_alias.json:/home/robotarium/Git/RobotariumRepositories/robotarium/serverBackend/mqttExtravaganza/tracker_alias.json \
+	--device=/dev/video0:/dev/video0 \
+	tracker
+
