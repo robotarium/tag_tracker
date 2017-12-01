4526e5975573f555fa0da267f0b9d3093730440d
dpickem <dpickem3@gatech.edu>
2017-12-01 10:04:35 -0500

    Adding testing

--- docker/run.sh
+++ docker/run.sh
@@ -2,9 +2,8 @@
 
 # Make sure that 'sudo xhost +' has been run before this script!!!!
 
-docker run -ti -d \
+docker run --rm -ti \
 	-e DISPLAY=$DISPLAY \
 	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-	-v /home/robotarium/Git/RobotariumRepositories/robotarium/serverBackend/mqttExtravaganza/tracker_alias.json:/home/robotarium/Git/RobotariumRepositories/robotarium/serverBackend/mqttExtravaganza/tracker_alias.json \
 	--device=/dev/video0:/dev/video0 \
-	tracker
+	tracker_test
