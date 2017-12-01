d5af1b9bedb74cde46ee16bfbfc3cd678b33ba99
dpickem <dpickem3@gatech.edu>
2017-12-01 10:13:56 -0500

    provide video path for test

--- scripts/detect_markers_test.sh
+++ scripts/detect_markers_test.sh
@@ -12,4 +12,4 @@
 # - rM 		flag that determines whether markers are rendered into output video
 # - m		use metric coordinates in output messages (versus pixel coordinates)
 
-../build/detectMarkers -m -sd=true -ad=true -bb=true -s=1.0 -p=1884 -d=0 -ci=0 -v=../out.mp4 -c=../data/camera.yml -l=0.026 -dp=../data/detector_params.yml -rm=../data/reference_markers_setup.yml
+../build/detectMarkers -m -sd=true -ad=true -bb=true -s=1.0 -p=1884 -d=0 -ci=0 -v=$1 -c=../data/camera.yml -l=0.026 -dp=../data/detector_params.yml -rm=../data/reference_markers_setup.yml
